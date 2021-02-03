5b115e4eeb45257e9868ea0985645ce427bef9de
MLY0813 <674740416@qq.com>
2021-02-03 13:09:14 +0800

    Delete contracts/Iface directory
    delete Iface

--- contracts/Iface/IMortgagepool.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.6.12;
-
-interface IMortgagePool {
-    function create(address pToken, address insurance, address underlying) external;
-}
